851397bbe4136e5f94fa75cec7f8ce4576803062
David Staudt <dstaudt@usa.net>
2024-12-04 00:07:57 -0600

    Rebasing

---
+++ debian/nautilus-uri-bookmark.doc-base.ex
@@ -0,0 +1,20 @@
+Document: nautilus-uri-bookmark
+Title: Debian nautilus-uri-bookmark Manual
+Author: <insert document author here>
+Abstract: This manual describes what nautilus-uri-bookmark is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/nautilus-uri-bookmark/nautilus-uri-bookmark.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/nautilus-uri-bookmark/nautilus-uri-bookmark.ps.gz
+
+Format: text
+Files: /usr/share/doc/nautilus-uri-bookmark/nautilus-uri-bookmark.text.gz
+
+Format: HTML
+Index: /usr/share/doc/nautilus-uri-bookmark/html/index.html
+Files: /usr/share/doc/nautilus-uri-bookmark/html/*.html
