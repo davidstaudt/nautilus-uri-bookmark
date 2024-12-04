e11466fa0e71b7a811319e4b1b8d7cddb472d43d
David Staudt <dstaudt@usa.net>
2024-12-03 21:57:20 -0600

    Remove GTK3 support Include scheme in initial file name Few cleanups and shortcuts

--- nautilus-uri-bookmark.py
+++ nautilus-uri-bookmark.py
@@ -1,23 +1,16 @@
 # nautilus-uri-bookmark
 #
-# Save web URIs to a file with a .uri extension that can be launched to visit the URL.
+# Save URIs to a file with a .uri extension that can be launched via xdg-open
+# when the bookmark file is opened.
 #
 # Ensure the python-nautilus package is installed
 # Place ~/.local/share/nautilus-python/extensions/, then restart Nautilus
 
+from gi import require_version
+require_version("Gdk", "4.0")
+from gi.repository import Nautilus, GObject, Gdk
+
 import os
-from gi import require_version, get_required_version
-
-try:
-    require_version("Gdk", "4.0")
-except ValueError:
-    try:
-        require_version("Gdk", "3.0")
-    except ValueError:
-        raise ImportError
-gdk_version = get_required_version("Gdk")
-
-
 from urllib.parse import urlparse
 
 if os.getenv("NAUTILUS_PYTHON_DEBUG") == "misc":
@@ -27,107 +20,42 @@ if os.getenv("NAUTILUS_PYTHON_DEBUG") == "misc":
     print("Waiting for debugger attach")
     debugpy.wait_for_client()
 
-if gdk_version == "3.0":
+class UriBookmarkMenuProvider(GObject.GObject, Nautilus.MenuProvider):
+    def __init__(self):
+        super().__init__()
+        self.clipboard = Gdk.Display.get_clipboard(Gdk.Display.get_default())
 
-    require_version("Gtk", "3.0")
-    from gi.repository import Nautilus, GObject, Gdk, Gtk
+    def save_uri(self, menu, inode):
 
-    class UriBookmarkMenuProvider(GObject.GObject, Nautilus.MenuProvider):
-        def __init__(self):
-            super().__init__()
-            self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
-            self.inode = None
-
-        def save_uri(self, menu, inode):
-            self.inode = inode
-            text = self.clipboard.wait_for_text()
+        def read_text_async_cb(clipboard, result):
             try:
-                link = urlparse(text)
+                link = urlparse(clipboard.read_text_finish(result))
             except:
                 return []
-            if link.scheme not in ["http", "https"]:
-                return []
 
-            fileText = link.geturl()
+            fileName = f"{link.scheme.upper()}: {link.netloc}"
 
-            fileName = f"Link to {link.netloc}"
-            fileName = "".join(x for x in fileName if (x.isalnum() or x in ":&[]@._- "))
-
-            basePath = self.inode.get_location().get_path()
-            if os.path.isfile(basePath):
-                return
-            if not os.path.exists(basePath):
+            basePath = inode.get_location().get_path()
+            if not os.path.exists(basePath) or os.path.isfile(basePath):
                 return
             instance = 0
             path = os.path.join(basePath, fileName + ".uri")
-            if os.path.exists(path):
-                while True:
-                    instance += 1
-                    path = os.path.join(basePath, f"{fileName}({str(instance)}).uri")
-                    if not os.path.exists(path):
-                        break
+            while os.path.exists(path):
+                instance += 1
+                path = os.path.join(basePath, f"{fileName}({str(instance)}).uri")
+
             with open(path, "w") as file:
-                file.write(fileText)
+                file.write(link.geturl())
 
-        def get_background_items(self, window, inode):
-            item = Nautilus.MenuItem(
-                name="nautilus_uri_bookmark",
-                label="Create URI shortcut",
-                tip="Create shortcut file from clipboard web URI",
-            )
-            item.connect("activate", self.save_uri, inode)
-            return [item]
+        self.clipboard.read_text_async(None, read_text_async_cb)
 
-else:
-    
-    require_version("Gtk", "4.0")
-    from gi.repository import Nautilus, GObject, Gdk, Gtk
-    
-    class UriBookmarkMenuProvider(GObject.GObject, Nautilus.MenuProvider):
-        def __init__(self):
-            super().__init__()
-            self.clipboard = Gdk.Display.get_clipboard(Gdk.Display.get_default())
-            self.inode = None
+    def get_background_items(self, inode):
 
-        def save_uri(self, menu, inode):
-            self.inode = inode
-            self.clipboard.read_text_async(None, self.read_text_async_cb)
-
-        def read_text_async_cb(self, clipboard, result):
-            text = clipboard.read_text_finish(result)
-            try:
-                link = urlparse(text)
-            except:
-                return []
-            if link.scheme not in ["http", "https"]:
-                return []
-
-            fileText = link.geturl()
-
-            fileName = f"Link to {link.netloc}"
-            fileName = "".join(x for x in fileName if (x.isalnum() or x in ":&[]@._- "))
-
-            basePath = self.inode.get_location().get_path()
-            if os.path.isfile(basePath):
-                return
-            if not os.path.exists(basePath):
-                return
-            instance = 0
-            path = os.path.join(basePath, fileName + ".uri")
-            if os.path.exists(path):
-                while True:
-                    instance += 1
-                    path = os.path.join(basePath, f"{fileName}({str(instance)}).uri")
-                    if not os.path.exists(path):
-                        break
-            with open(path, "w") as file:
-                file.write(fileText)
-
-        def get_background_items(self, inode):
-            item = Nautilus.MenuItem(
-                name="nautilus_uri_bookmark",
-                label="Create URI shortcut",
-                tip="Create shortcut file from clipboard web URI",
-            )
-            item.connect("activate", self.save_uri, inode)
-            return [item]
+        item = Nautilus.MenuItem(
+            name="nautilus_uri_bookmark",
+            label="Create URI shortcut",
+            tip="Create shortcut file from clipboard web URI",
+        )
+        item.connect("activate", self.save_uri, inode)
+        
+        return [item]
